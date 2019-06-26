343210d5e13364123db6561d00a20407174d3ad9
Marco-Senpai <mcneilmarcus92@gmail.com>
2019-06-26 18:04:03 -0500

    2nd commit

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -2,6 +2,7 @@ package org.launchcode.cheesemvc.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -11,7 +12,8 @@ public class CheeseController {
 
     // request path: /cheese
     @RequestMapping(value = "")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("title", "My Cheeses");
         return "cheese/index";
     }
 }
